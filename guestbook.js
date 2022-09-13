5a756a5153edeeef669348d872631d67b4c3c5f6
John Gray <100069417+anchorGray@users.noreply.github.com>
2022-09-12 20:47:11 -0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/e10a4860-3669-40a1-b55f-22d6be24352e/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/read_guestbook_entries_sequence`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/save_guestbook_entry_sequence`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
